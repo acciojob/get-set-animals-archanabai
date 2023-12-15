202585117c4456ea25c2833bfa399b279c4fc40c
archanabai <121252186+archanabai@users.noreply.github.com>
2023-12-15 11:34:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,43 @@
-//complete this code
-class Animal {}
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  makeSound() {
+    console.log("Generic animal sound");
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage:
+
+const genericAnimal = new Animal("Unknown");
+console.log(genericAnimal.species); // Output: Unknown
+genericAnimal.makeSound(); // Output: Generic animal sound
+
+const fluffy = new Cat("Cat");
+console.log(fluffy.species); // Output: Cat
+fluffy.makeSound(); // Output: Generic animal sound
+fluffy.purr(); // Output: purr
+
+const buddy = new Dog("Dog");
+console.log(buddy.species); // Output: Dog
+buddy.makeSound(); // Output: Generic animal sound
+buddy.bark(); // Output: woof
+
 window.Cat = Cat;
